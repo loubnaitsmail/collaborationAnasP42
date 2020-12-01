3b85ee667f232f662088b033c9873c90dd511f85
Loubna Itsmail <loubna.itsmail.1993@gmail.com>
2020-12-01 18:31:51 +0100

    test

--- 1A_42/libft/ft_bzero.c
+++ 1A_42/libft/ft_bzero.c
@@ -1,20 +1,23 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_bzero.c                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: litsmail <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/12/01 18:23:48 by litsmail          #+#    #+#             */
+/*   Updated: 2020/12/01 18:24:37 by litsmail         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
 
 #include "libft.h"
 
-void ft_bzero(void *str, size_t size)
+void	ft_bzero(void *str, size_t size)
 {
-    size_t i;
-    unsigned char *copy;
-    
-    if (size == 0)
-        return;
-    ft_memset(str, 0, size);
-    
-   /*/i = 0;
-    copy = str;
-    while (i < size)
-    {
-        copy[i] = '0';
-        i++;
-    }*/
+	size_t		i;
+	unsigned char	*copy;
+
+	if (size == 0)
+		return;
+	ft_memset(str, 0, size);
 }
